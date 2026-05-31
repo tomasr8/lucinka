23c5c380e54fdcd987d797c4b75fd21133c6a3bf
Anna <ferranna87@yahoo.it>
2026-05-31 14:08:07 +0200

    Fix diary ui

--- src/Diary.jsx
+++ src/Diary.jsx
@@ -214,7 +214,7 @@ const DiaryCard = forwardRef(function DiaryCard(
               {isAdmin && (
                 <button
                   onClick={() => handleDeleteMedia(m.id)}
-                  className="absolute top-1 right-1 p-1 bg-black/60 text-white rounded-full opacity-0 group-hover:opacity-100 transition-opacity"
+                  className="absolute top-1 right-1 p-1 bg-black/60 text-white rounded-full opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity"
                 >
                   <X className="w-3 h-3" />
                 </button>
